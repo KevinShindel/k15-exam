44c8170b973ea10770d79dc24e5b72afe551724e
KevinShindel <kevin.shindel@yahoo.com>
2018-10-16 17:40:01 +0300

    Exam Work

---
+++ js/init.js
@@ -0,0 +1,17 @@
+$('.sl').slick({
+  cssEase: 'linear',
+  autoplay: true,
+  autoplaySpeed: 4500,
+  dots: true,
+  infinite: true,
+  fade: true,
+  arrows: true
+});
+
+$('.sl2').slick({
+  autoplay: true,
+  autoplaySpeed: 2500,
+  arrows: false,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+});
